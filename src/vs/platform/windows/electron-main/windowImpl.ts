e4bbec3f3de9f9eafd4c0def8395cb8189927359
deepak1556 <hop2deep@gmail.com>
2025-02-20 22:54:29 +0900

    chore: log js stacks from unresponsive window

--- src/vs/platform/windows/electron-main/windowImpl.ts
+++ src/vs/platform/windows/electron-main/windowImpl.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import electron, { BrowserWindowConstructorOptions } from 'electron';
-import { DeferredPromise, RunOnceScheduler, timeout } from '../../../base/common/async.js';
+import { DeferredPromise, RunOnceScheduler, timeout, Delayer } from '../../../base/common/async.js';
 import { CancellationToken } from '../../../base/common/cancellation.js';
 import { toErrorMessage } from '../../../base/common/errorMessage.js';
 import { Emitter, Event } from '../../../base/common/event.js';
@@ -540,6 +540,10 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 	private pendingLoadConfig: INativeWindowConfiguration | undefined;
 	private wasLoaded = false;
 
+	private _jsCallStackCollector: Delayer<void>;
+	private _jsCallStackMap: Map<string, number>;
+	private _jsCallStackCollectorStopScheduler: RunOnceScheduler;
+
 	constructor(
 		config: IWindowCreationOptions,
 		@ILogService logService: ILogService,
@@ -594,6 +598,13 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 			this._lastFocusTime = Date.now(); // since we show directly, we need to set the last focus time too
 		}
 		//#endregion
+
+		this._jsCallStackCollector = new Delayer<void>(1000);
+		this._jsCallStackMap = new Map<string, number>();
+		// Stop collecting after 15s max
+		this._jsCallStackCollectorStopScheduler = this._register(new RunOnceScheduler(() => {
+			this.stopCollectingJScallStacks();
+		}, 15000));
 
 		// respect configured menu bar visibility
 		this.onConfigurationUpdated();
@@ -655,6 +666,7 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 
 		// Window error conditions to handle
 		this._register(Event.fromNodeEventEmitter(this._win, 'unresponsive')(() => this.onWindowError(WindowError.UNRESPONSIVE)));
+		this._register(Event.fromNodeEventEmitter(this._win, 'responsive')(() => this.onWindowError(WindowError.RESPONSIVE)));
 		this._register(Event.fromNodeEventEmitter(this._win.webContents, 'render-process-gone', (event, details) => details)(details => this.onWindowError(WindowError.PROCESS_GONE, { ...details })));
 		this._register(Event.fromNodeEventEmitter(this._win.webContents, 'did-fail-load', (event, exitCode, reason) => ({ exitCode, reason }))(({ exitCode, reason }) => this.onWindowError(WindowError.LOAD, { reason, exitCode })));
 
@@ -730,6 +742,7 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 	}
 
 	private async onWindowError(error: WindowError.UNRESPONSIVE): Promise<void>;
+	private async onWindowError(error: WindowError.RESPONSIVE): Promise<void>;
 	private async onWindowError(error: WindowError.PROCESS_GONE, details: { reason: string; exitCode: number }): Promise<void>;
 	private async onWindowError(error: WindowError.LOAD, details: { reason: string; exitCode: number }): Promise<void>;
 	private async onWindowError(type: WindowError, details?: { reason?: string; exitCode?: number }): Promise<void> {
@@ -740,6 +753,9 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 				break;
 			case WindowError.UNRESPONSIVE:
 				this.logService.error('CodeWindow: detected unresponsive');
+				break;
+			case WindowError.RESPONSIVE:
+				this.logService.error('CodeWindow: recovered from unresponsive');
 				break;
 			case WindowError.LOAD:
 				this.logService.error(`CodeWindow: failed to load (reason: ${details?.reason || '<unknown>'}, code: ${details?.exitCode || '<unknown>'})`);
@@ -799,6 +815,12 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 						return;
 					}
 
+					// Interrupt V8 and collect JavaScript stack
+					this._jsCallStackCollector.trigger(() => this.startCollectingJScallStacks());
+					if (!this._jsCallStackCollectorStopScheduler.isScheduled()) {
+						this._jsCallStackCollectorStopScheduler.schedule();
+					}
+
 					// Show Dialog
 					const { response, checkboxChecked } = await this.dialogMainService.showMessageBox({
 						type: 'warning',
@@ -815,6 +837,7 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 					// Handle choice
 					if (response !== 2 /* keep waiting */) {
 						const reopen = response === 0;
+						this.stopCollectingJScallStacks();
 						await this.destroyWindow(reopen, checkboxChecked);
 					}
 				}
@@ -846,6 +869,9 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 					const reopen = response === 0;
 					await this.destroyWindow(reopen, checkboxChecked);
 				}
+				break;
+			case WindowError.RESPONSIVE:
+				this.stopCollectingJScallStacks();
 				break;
 		}
 	}
@@ -958,6 +984,36 @@ export class CodeWindow extends BaseWindow implements ICodeWindow {
 				electron.app.setProxy({ proxyRules, proxyBypassRules, pacScript: '' });
 			}
 		}
+	}
+
+	private async startCollectingJScallStacks(): Promise<void> {
+		if (!this._jsCallStackCollector.isTriggered()) {
+			const stack = await this._win.webContents.mainFrame.collectJavaScriptCallStack();
+			// Increment the count for this stack trace
+			if (stack) {
+				const count = this._jsCallStackMap.get(stack) || 0;
+				this._jsCallStackMap.set(stack, count + 1);
+			}
+			this._jsCallStackCollector.trigger(() => this.startCollectingJScallStacks());
+		}
+	}
+
+	private async stopCollectingJScallStacks(): Promise<void> {
+		this._jsCallStackCollector.dispose();
+		if (this._jsCallStackMap.size) {
+			let logMessage = `CodeWindow unresponsive samples:\n`;
+			let samples = 0;
+			const sortedEntries = Array.from(this._jsCallStackMap.entries())
+				.sort((a, b) => b[1] - a[1]);
+			for (const [stack, count] of sortedEntries) {
+				samples += count;
+				logMessage += `<${count}> ${stack}\n`;
+			}
+			logMessage += `Total Samples: ${samples}\n`;
+			this.logService.error(logMessage);
+		}
+		this._jsCallStackMap.clear();
+
 	}
 
 	addTabbedWindow(window: ICodeWindow): void {
